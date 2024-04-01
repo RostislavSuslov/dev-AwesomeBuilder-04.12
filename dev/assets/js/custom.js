c2fb76bdbf759f69dfc825546c3262915707544c
RostSuslov <suslovros@gmail.com>
2024-04-01 21:07:51 +0300

    feat: creat build plan layout

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -1,1 +1,45 @@
-  
+const planItem = document.querySelectorAll('.item-path')
+const cellAddress = document.querySelector('#address')
+const cellFloors = document.querySelector('#floors')
+const cellFreeFlat = document.querySelector('#freeFlat')
+const cellSoldFlat = document.querySelector('#soldFlat')
+planItem.forEach(build => {
+    build.addEventListener('mouseover',()=> {
+		const thisAddress = build.getAttribute('data-adress')
+		const thisFloors = build.getAttribute('data-floors')
+		const thisFreeFlats = build.getAttribute('data-free-flats')
+		const thisSoldFlats = build.getAttribute('data-sold-flats')
+
+
+		cellAddress.innerText = thisAddress
+		cellFloors.innerText = thisFloors
+		cellFreeFlat.innerText = thisFreeFlats
+		cellSoldFlat.innerText = thisSoldFlats
+	})
+
+	const buildFreeFlats = build.getAttribute('data-free-flats')
+
+/*	 buildFreeFlats === "0" ? build.classList.add('sold') : null
+
+
+	if (build.classList.contains('sold')) {
+		const buildLink= build.closest('.item-link')
+
+		buildLink.addEventListener('click', (event)=>{
+			event.preventDefault()
+		})
+	}*/
+
+	if( buildFreeFlats === "0") {
+		build.style.cssText = `
+		    fill: #ff8f8f;
+	  		stroke: #940000;
+		`;
+
+		const buildLink= build.closest('.item-link')
+
+		buildLink.addEventListener('click', (event)=>{
+			event.preventDefault()
+		})
+	}
+})
